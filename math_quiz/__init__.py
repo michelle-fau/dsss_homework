743756f0ebc2d6f4230f1372d024d37f6960321b
michelle <chelle.michelle@fau.de>
2024-11-01 13:12:03 +0100

    adding init file

---
+++ math_quiz/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py for math_quiz package
+
+from .math_quiz import generate_random_int, generate_random_operator, create_math_problem, math_quiz
+
+__all__ = ["generate_random_int", "generate_random_operator", "create_math_problem", "math_quiz"]
